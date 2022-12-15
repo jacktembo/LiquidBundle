a987cd3db1d6b12873a054b7c7d3c2e911747182
jacktembo <jack@jacktembo>
2022-12-15 20:13:48 +0200

    adding passenger file

--- LiquidBundle/settings.py
+++ LiquidBundle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3rjvq@--_q%yy_*m*+c@zv=3*$olypp(#%ttw0t__ow+-kwah(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
